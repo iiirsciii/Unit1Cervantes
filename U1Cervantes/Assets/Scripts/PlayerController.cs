cc8d8f9c352e9b4648e605c683c986145b567c66
iiirsciii <722997@student.dusd.net>
2025-03-10 10:21:12 -0700

    finished project almost
    need to added bonus parts and added turning to my car

--- U1Cervantes/Assets/Scripts/PlayerController.cs
+++ U1Cervantes/Assets/Scripts/PlayerController.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    // Private Variables
+    private float speed = 5.0f;
+    private float turnSpeed = 25.0f;
+    private float horizontal1Input;
+    private float fowardInput;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,7 +19,13 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // this is where we get player input
+        horizontal1Input = Input.GetAxis("Horizontal");
+        fowardInput = Input.GetAxis("Vertical");
+
         // We'll move this vecihle foward
-        transform.Translate(Vector3.forward);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * fowardInput);
+        // We turn the vehicle
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontal1Input); 
     }
 }
